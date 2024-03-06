79f0d6d4ee7f6bc4550338e713772642d537659c
Dev-LucasNunes <dev.lucasnunes@gmail.com>
2024-03-06 12:54:57 -0300

    upadate

--- lib/Algoritmos/bubble_sort.rb
+++ lib/Algoritmos/bubble_sort.rb
@@ -1,13 +1,16 @@
-i = 0
-while i < input.length - 1
-  j = 0
-  while j < input.length - i - 1
-    if input[j] > input[j + 1]
-      temp = input[j]
-      input[j] = input[j + 1]
-      input[j + 1] = temp
-    end
-    j += 1
+
+end
+def bubble_sort(array)
+  i = 0
+  while i < input.length - 1
+    j = 0
+    while j < input.length - i - 1
+      if input[j] > input[j + 1]
+        temp = input[j]
+        input[j] = input[j + 1]
+        input[j + 1] = temp
+      end
+      j += 1
   end
   i += 1
 end
