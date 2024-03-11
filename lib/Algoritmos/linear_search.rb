3745b735b8419b22c15570f4d449cf1d3ab35d78
Dev-LucasNunes <dev.lucasnunes@gmail.com>
2024-03-10 21:14:40 -0300

    update

--- lib/Algoritmos/linear_search.rb
+++ lib/Algoritmos/linear_search.rb
@@ -2,10 +2,9 @@ def linear_search(array, key)
   i = 0
   while i < array.length
     if array[i] == key
-      return i, key
+      return key
     end
     i += 1
   end
   puts "a chave #{key} não foi encontrada"
 end
-
